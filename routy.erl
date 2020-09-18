dd4f8ff387219d755eb1e4404fdb4a70c7e5b142
Simon Binyamin <simonbinyamin@live.com>
2020-09-18 12:17:00 +0200

    ff

--- routy.erl
+++ routy.erl
@@ -36,10 +36,6 @@ router(Name, N, Hist, Intf, Table, Map) ->
 			Intf1 = intf:remove(Down, Intf),
 			router(Name, N, Hist, Intf1, Table, Map);
  
-		{status, From} ->
-			From ! {status, {Name, N, Hist, Intf, Table, Map}},
-			router(Name, N, Hist, Intf, Table, Map);
- 
 		{links, Node, R, Links} ->
 			case hist:update(Node, R, Hist) of
 				{new, Hist1} ->
@@ -58,7 +54,40 @@ router(Name, N, Hist, Intf, Table, Map) ->
 			Message = {links, Name, N, intf:list(Intf)},
 			intf:broadcast(Message, Intf),
 			router(Name, N+1, Hist, Intf, Table, Map);
- 
+			% matches only when the destination is Name (the router itself). This means the message has reached its final destination.
+		{route, Name, _From, Message} ->
+			io:format("~w: received message ~w ~n", [Name, Message]),
+			router(Name, N, Hist, Intf, Table, Map);
+		%The second one covers the case when the message has not reached its final destination and needs to be forwarded by the router.
+		{route, To, From, Message} ->
+			io:format("~w: routing message (~w)", [Name, Message]),
+			case dijkstra:route(To, Table) of
+			{ok, Gw} ->
+				case intf:lookup(Gw, Intf) of
+				{ok, Pid} ->
+					Pid ! {route, To, From, Message};
+				notfound ->
+					ok
+				end;
+			notfound ->
+				ok
+			end,
+			router(Name, N, Hist, Intf, Table, Map);
+			
+		%send is just a shortcut for sending a message from one router to another. 
+		%For example, to send the message "hello" from a router r1 to r2, you could either write either of the following:
+		%r1 ! {route, r2, r1, "hello"}
+		%r1 ! {send, r2, "hello"}
+		{send, To, Message} ->
+			self() ! {route, To, Name, Message},
+			router(Name, N, Hist, Intf, Table, Map);
+		{status, From} ->
+			From ! {status, {Name, N, Hist, Intf, Table, Map}},
+			router(Name, N, Hist, Intf, Table, Map);
+
 		stop ->
-			ok
+			ok;
+		status ->
+			io:format("Name: ~w\n N: ~w\nHistory: ~w\nInterfaces: ~w\nTable: ~w\nMap: ~w\n", [Name, N, Hist, Intf, Table, Map]),
+			router(Name, N, Hist, Intf, Table, Map)
 	end.
